05551de81558f2cc76ccb6f7bcf6f6cc65042c27
gggumiiinqiii <1378827345@qq.com>
2024-01-02 22:23:26 +0800

    feat: 二维降低到了1维

--- 数组和字符串/6.z字形变换.js
+++ 数组和字符串/6.z字形变换.js
@@ -4,49 +4,31 @@
  * @return {string}
  */
 var convert = function (s, numRows) {
-  if (numRows == 1) {
+  if (numRows === 1 || numRows >= s.length) {
     return s;
   }
-  let depulate = numRows + numRows - 2; //经历这样一次重复 4
-  let xx = Math.ceil(s.length / depulate); //需要重复几次
-  let k = 0;
-  let i = 0;
-  // 14 3
-  let dp = Array.from(
-    Array.from(Array(numRows), () =>
-      Array(Math.ceil(s.length / numRows) + numRows).fill(0)
-    )
-  );
 
-  for (let j = 0; j < xx; j++) {
-    let kkk = numRows - 1;
-    let kkkl = k;
-    for (let llg = 0; llg < depulate; llg++) {
-      if (s[i] == undefined) {
-        break;
-      }
-      if (llg < numRows) {
-        dp[llg][k] = s[i++];
-      } else {
-        dp[--kkk][++kkkl] = s[i++];
-      }
-    }
+  const result = []; // 用于存储最终结果的数组
+  const depulate = numRows + numRows - 2; // 每个完整重复段的长度
 
-    k = k + numRows - 1;
-  }
-  k++;
-
-  console.table(dp);
-  let cc = "";
   for (let i = 0; i < numRows; i++) {
-    for (let j = 0; j < s.length; j++) {
-      if (dp[i][j]) {
-        cc = cc + dp[i][j];
+    let j = i;
+    while (j < s.length) {
+      result.push(s[j]); // 将当前字符添加到结果数组中
+      const nextJ = j + depulate - 2 * i;
+
+      // 如果不是第一行和最后一行，并且下一个字符的索引在字符串长度范围内，将下一个字符添加到结果数组中
+      if (i !== 0 && i !== numRows - 1 && nextJ < s.length) {
+        result.push(s[nextJ]);
       }
+
+      j += depulate; // 移动到下一个重复段的起始位置
     }
   }
-  console.log(cc);
-  return cc;
+
+  const resultString = result.join(""); // 将结果数组转换为字符串
+  console.log(resultString);
+  return resultString;
 };
 
 module.exports = convert;
---
+++ 测试用例/数组和字符串/6.z字形变化的.test.js
@@ -0,0 +1,5 @@
+const convert = require("../../数组和字符串/6.z字形变换");
+convert("PAYPALISHIRING", 3); //PAHNAPLSIIGYIR
+convert("PAYPALISHIRING", 4); //PINALSIGYAHRPI
+convert("PAYPALISHIRING", 5); //PHASIYIRPLIGAN
+// convert("sss", 2);
