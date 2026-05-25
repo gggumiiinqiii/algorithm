131a6624bd83ee58207801fe51c043ec282c6800
gggumiiinqiii <1378827345@qq.com>
2026-05-26 02:10:13 +0800

    feat: 重写两数相加

--- 链表/2.两数相加.js
+++ 链表/2.两数相加.js
@@ -12,27 +12,37 @@ const ListNode = require("../测试用例/链表/linkList");
  * @return {ListNode}
  */
 function addTwoNumbers(l1, l2) {
-  let dummyHead = new ListNode(0); // 哑节点，简化边界条件处理
-  let current = dummyHead; // 用来构建结果链表
-  let carry = 0; // 进位
-
-  while (l1 !== null || l2 !== null || carry !== 0) {
-    // 获取当前位的值
-    let val1 = l1 !== null ? l1.val : 0;
-    let val2 = l2 !== null ? l2.val : 0;
-
-    // 计算当前位的和及进位
-    let sum = val1 + val2 + carry;
-    carry = Math.floor(sum / 10); // 计算进位
-    current.next = new ListNode(sum % 10); // 记录当前位
-    console.log(sum);
-    // 移动指针
-    current = current.next;
-    if (l1 !== null) l1 = l1.next;
-    if (l2 !== null) l2 = l2.next;
+  //一个虚拟头节点，如果没有虚拟头节点就需要判断，当前节点是不是头节点
+  let dummy = new ListNode(0);
+  let p = dummy;
+  let c = 0;
+  while (l1 && l2) {
+    c += l1.val + l2.val;
+    p.next = new ListNode(c % 10);
+    c = Math.floor(c / 10);
+    p = p.next;
+    l1 = l1.next;
+    l2 = l2.next;
   }
-  console.log(dummyHead);
-  return dummyHead.next; // 返回哑节点的下一个节点，即结果链表的头
+  while (l1) {
+    c += l1.val;
+    p.next = new ListNode(c % 10);
+    c = Math.floor(c / 10);
+    p = p.next;
+    l1 = l1.next;
+  }
+  while (l2) {
+    c += l2.val;
+    p.next = new ListNode(c % 10);
+    c = Math.floor(c / 10);
+    p = p.next;
+    l2 = l2.next;
+  }
+  if (c) {
+    p.next = new ListNode(c);
+  }
+  console.log(dummy.next);
+  return dummy.next;
 }
 
 module.exports = addTwoNumbers;
