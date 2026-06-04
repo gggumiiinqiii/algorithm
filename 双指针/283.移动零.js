53c2b02e05cab723c92949822739eee32899e771
gggumiiinqiii <1378827345@qq.com>
2026-06-04 16:40:27 +0800

    feat: 移动0的暴力解法，双重for循环

--- 双指针/283.移动零.js
+++ 双指针/283.移动零.js
@@ -3,16 +3,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-  let index = 0;
-  let n = 0;
-  //0 1 0 3 12
-  // 定义一个临时变量表示当前数值，定义一个index索引
-  // 如果当前值不为0，取出当前变量、然后当前值置为0，索引0设置为当前数值，然后遍历可得
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      n = nums[i];
-      nums[i] = 0;
-      nums[index++] = n;
+  let n = nums.length;
+  // let left = 0;
+  // let right = 0;
+  for (let i = n; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (nums[j] == 0) {
+        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+      }
     }
   }
   console.log(nums);
